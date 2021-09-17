5dddc231a05cce8c30c4984eaadcc164bb0dcee7
eteimz <8teims@gmail.com>
2021-09-17 16:13:11 +0100

    Completed chapter 2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <h1> Hello World! </h1>
+  <div id="app">
+    <h1>My To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script>
 
-export default {
-  name: 'App',
-}
+
+<script>
+  import ToDoItem from './components/ToDoItem.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      ToDoItem
+    }
+  }
 </script>
 
 <style>
