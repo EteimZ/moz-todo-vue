5a36b5d7cdf4bdf40a05ea381008b7b6e172e1c6
eteimz <8teims@gmail.com>
2021-09-18 12:37:49 +0100

    Added \'todo item edit form\'

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
     <h2 id="list-summary">{{listSummary}}</h2>
     <ul aria-labelledby="list-summary" class="stack-large">
       <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done" :id="item.id" @checkbox-changed="updateDoneStatus(item.id)"></to-do-item>
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"
+            @checkbox-changed="updateDoneStatus(item.id)"
+            @item-deleted="deleteToDo(item.id)"
+            @item-edited="editToDo(item.id, $event)">
+        </to-do-item>
       </li>
     </ul>
   </div>
@@ -44,8 +48,17 @@
       updateDoneStatus(toDoId) {
         const toDoToUpdate = this.ToDoItems.find(item => item.id === toDoId)
         toDoToUpdate.done = !toDoToUpdate.done
-      }
+      },
 
+      deleteToDo(toDoId) {
+        const itemIndex = this.ToDoItems.findIndex(item => item.id === toDoId);
+        this.ToDoItems.splice(itemIndex, 1);
+      },
+
+      editToDo(toDoId, newLabel) {
+        const toDoToEdit = this.ToDoItems.find(item => item.id === toDoId);
+        toDoToEdit.label = newLabel;
+      }
     },
 
     computed: {
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="custom-checkbox">
-    <input type="checkbox" :id="id" :checked="isDone" class="checkbox"
-      @change="$emit('checkbox-changed')"/>
-    <label :for="id" class="checkbox-label">{{label}}</label>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input type="checkbox" class="checkbox" :id="id" :checked="isDone"
+             @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn"  @click="toggleToItemEditForm">
+        Edit <span class="visually-hidden">{{label}}</span>
+      </button>
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
   </div>
+  <to-do-item-edit-form v-else :id="id" :label="label"
+                      @item-edited="itemEdited"
+                      @edit-cancelled="editCancelled">
+  </to-do-item-edit-form>
 </template>
 
-
 <script>
+import ToDoItemEditForm from "./ToDoItemEditForm";
 
 export default {
   props: {
@@ -15,11 +29,35 @@ export default {
     done: { default: false, type: Boolean },
     id: {required: true, type: String}
   },
+  components: {
+    ToDoItemEditForm
+  },
   data() {
     return {
-      isDone: this.done,
+      isEditing: false
     };
+  },
+  computed: {
+    isDone() {
+      return this.done;
+    }
+  },
+  methods: {
+    deleteToDo() {
+      this.$emit('item-deleted');
+    },
+    toggleToItemEditForm() {
+      this.isEditing = true;
+    },
+    itemEdited(newLabel) {
+      this.$emit('item-edited', newLabel);
+      this.isEditing = false;
+    },
+    editCancelled() {
+      this.isEditing = false;
+    }
   }
+
 };
 </script>
 
